cd7bfd56404cfe4c47d0d2198cd1312e5fa1618d
pozi119 <pozi119@163.com>
2015-07-10 12:13:36 +0800

    Initial Commit

---
+++ VOVCManager.Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VOVCManager.Swift
+//
+//  Created by Valo on 15/7/10.
+//  Copyright (c) 2015年 Valo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
